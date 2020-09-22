38a870df80907b9633bfff3aba40029e24c7923c
strelkov <hablebuble@gmail.com>
2020-09-22 13:05:54 +0300

    update

--- services/create_subgroup__rus.py
+++ services/create_subgroup__rus.py
@@ -1,9 +1,7 @@
 from models.subgroup_rus import SubgroupRus
 import pandas as pd
 
-from datetime import datetime
 from mongoengine import *
-
 
 connect(
     db='catalogue',
@@ -16,8 +14,6 @@ file = 'subgroup_rus.xlsx'
 exel_data_df = pd.read_excel('../data/{}'.format(file), dtype={'subgroup_rus': str})
 
 subgroup_rus_file = exel_data_df.to_dict(orient='record')
-
-print(subgroup_rus_file[:5])
 
 for i in subgroup_rus_file:
     insert = SubgroupRus(
